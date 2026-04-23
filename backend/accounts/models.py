6b118727c69ad4841a78d98f954f876e3a182e68
Sushma Vankhede <sushmavankhede24@gmail.com>
2026-04-23 17:00:07 +0200

    Add custom user model with wallet balance

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    wallet_balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'accounts',
+    'game',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
